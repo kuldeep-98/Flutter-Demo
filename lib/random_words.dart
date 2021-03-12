f3a6437a1fe244130346c41649f0942d788a9b20
kuldeep-98 <kuldeepbhama4455@gmail.com>
2021-03-13 03:04:05 +0530

    Final Commit

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -13,7 +13,7 @@ class RandomWordsState extends State<RandomWords> {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
       itemBuilder: (context,item){
-        if(item.isOdd)  return Divider();
+        if(item.isOdd)  return Divider(); 
 
         final index = item ~/ 2;
 
@@ -26,13 +26,58 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair){
-    return ListTile(title: Text(pair.asPascalCase,style: TextStyle(fontSize: 18),),);
+    final alreadySaved = _savedWordPair.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18),
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite:Icons.favorite_border,
+        color: alreadySaved?Colors.red:null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _savedWordPair.remove(pair);
+          }else{
+            _savedWordPair.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _savedWordPair.map((WordPair pair) {
+            return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16),), );
+          });
+          final List<Widget> devided = ListTile.divideTiles(tiles: tiles,context:context).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs'),
+            ),
+            body: ListView(children: devided,),
+          );
+        }
+      )
+    );
   }
 
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar( 
-        title:Text('WordPair Generator')
+        title:Text('WordPair Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildList(),
     );
